12e2c7003a02520f247d1364af3c377f2ab7e3e4
Daniel Pecos Martinez <me@danielpecos.com>
2022-07-19 21:02:56 +0200

    More client tests

--- client/client.test.ts
+++ client/client.test.ts
@@ -17,4 +17,24 @@ describe("Book API", () => {
 
     expect(books[0].id).not.toBeNull();
   });
+
+  it("should add a new book to the collection", async () => {
+    let response = await api.getBooks();
+    let books = response.data as api.Book[];
+    expect(books.length).toBe(1);
+
+    const newBook: api.Book = {
+      id: 2,
+      title: "New book",
+      author: "New author",
+      summary: "This is a test book",
+      rating: 3,
+    };
+    await api.postBooks(newBook);
+
+    response = await api.getBooks();
+    books = response.data as api.Book[];
+    expect(books.length).toBe(2);
+    expect(books[1]).toMatchObject(newBook);
+  });
 });
